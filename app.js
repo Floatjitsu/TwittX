f8620fba26d1d1df96b2181bf3e029632d899307
Timur Celik <timur.celik@hs-augsburg.de>
2019-11-26 14:43:47 +0100

    Added nasa api key to config.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Twit = require('twit');
 const config = require('./config.js');
 
-const T = new Twit(config);
+const T = new Twit(config.twitter);
 
 T.post('statuses/update', { status: 'Hello World! 🚀🐦' }, (err, data, reponse) => {
   console.log(data);
