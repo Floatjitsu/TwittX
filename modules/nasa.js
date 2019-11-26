40816aa0465879371ad8f661a92ac10f8a6f04c7
Timur Celik <timur.celik@hs-augsburg.de>
2019-11-26 17:13:43 +0100

    added pictures folder, download and post picture of the day from nasa twitter

--- modules/nasa.js
+++ modules/nasa.js
@@ -1,9 +1,19 @@
 const config = require('../config.js')
 const request = require('request');
+const fs = require('fs');
 
 const pictureOfTheDay = new Promise((resolve, reject) => {
-    request('https://api.nasa.gov/planetary/apod?api_key='+config.nasa.api_key , (error, response, body) => {
-        resolve(JSON.parse(body));
+    //First step: make the picture of the day api call
+    request('https://api.nasa.gov/planetary/apod?api_key=' + config.nasa.api_key, (error, response, body) => {
+        const url = JSON.parse(body).url;
+        const fileName = url.split('/').pop();
+        //Second step: save the picture to the pictures folder based on the returned url
+        request(url).pipe(fs.createWriteStream('./pictures/' + fileName)).on('close', () => {
+          const params = { encoding: 'base64' };
+          const b64 = fs.readFileSync('pictures/' + fileName, params);
+          //Last step: resolve the promise with the right object to upload media data to twitter
+          resolve({media_data: b64});
+        });
     });
 });
 
