3e2dc702f21638a6d70f23b3baac0f305b88d8b1
Timur Celik <timur.celik@gmx.de>
2019-12-02 18:33:39 +0100

    reject promise if media type is video and some format fixes

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const nasa = require('./modules/nasa.js');
 
 const T = new Twit(config.twitter);
 
-
 //Picture of the day post
 nasa.pictureOfTheDay.then(result => {
   //Upload the downloaded image to twitter
   T.post('media/upload', result, (err, data, reponse) => {
     //POST the picture URL on twitter
     T.post('statuses/update', {status: 'Here is your NASA picture of the day', media_ids: data.media_id_string}, (error, data2, reponse2) => {
-      console.log(data2);
+        console.log(data2);
     });
   });
 }).catch(err => {
--- modules/nasa.js
+++ modules/nasa.js
@@ -1,19 +1,25 @@
-const config = require('../config.js')
+const config = require('../config.js');
 const request = require('request');
 const fs = require('fs');
 
 const pictureOfTheDay = new Promise((resolve, reject) => {
     //First step: make the picture of the day api call
-    request('https://api.nasa.gov/planetary/apod?api_key=' + config.nasa.api_key, (error, response, body) => {
-        const url = JSON.parse(body).url;
-        const fileName = url.split('/').pop();
-        //Second step: save the picture to the pictures folder based on the returned url
-        request(url).pipe(fs.createWriteStream('./pictures/' + fileName)).on('close', () => {
-          const params = { encoding: 'base64' };
-          const b64 = fs.readFileSync('pictures/' + fileName, params);
-          //Last step: resolve the promise with the right object to upload media data to twitter
-          resolve({media_data: b64});
-        });
+    request('https://api.nasa.gov/planetary/apod?api_key=' + config.nasa.api_key /*+ "&&date=" + '2019-11-26'*/, (error, response, body) => {
+        const jsonBody = JSON.parse(body);
+        if(jsonBody.media_type === 'video') {
+            // TODO: post url instead of downloading and post a picture 
+            reject('media type is video!');
+        } else {
+            const url = jsonBody.url;
+            const fileName = url.split('/').pop();
+            //Second step: save the picture to the pictures folder based on the returned url
+            request(url).pipe(fs.createWriteStream('./pictures/' + fileName)).on('close', () => {
+                const params = { encoding: 'base64' };
+                const b64 = fs.readFileSync('pictures/' + fileName, params);
+                //Last step: resolve the promise with the right object to upload media data to twitter
+                resolve({media_data: b64});
+            });
+        }
     });
 });
 
