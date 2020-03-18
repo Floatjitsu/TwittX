15bc0bda5cdc31279ca74d7c96d2f9bef6e92212
Timur Celik <timur.celik@gmx.de>
2020-03-18 11:27:20 +0100

    build test environment for server on development branch

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const Scheduler = require('./modules/scheduler');
 const Twitter = require('./modules/twitter');
 
-// Scheduler.scheduleNasaPictureOfTheDayPost();
-// Scheduler.scheduleNasaNearestEarthObjectPost();
-// Scheduler.scheduleNasaMarsRoverPicturePost();
-// Scheduler.scheduleSpacexLatestLaunchPost();
-// Scheduler.scheduleSpacexNextLaunchPost();
-
-Twitter.makePictureOfTheDayPost();
+// Twitter.makePictureOfTheDayPost();
 // Twitter.makeNearestEarthObjectPost();
 // Twitter.makeLatestSpaceXLaunchPost();
 // Twitter.makeNextSpaceXLaunchPost();
-// Twitter.makeMarsRoverPicturePost();
+Twitter.makeMarsRoverPicturePost();
