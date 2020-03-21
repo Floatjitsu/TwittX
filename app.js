14372f7fad20291ed730dcf5bc25b5bbd15124b1
Timur Celik <timur.celik@gmx.de>
2020-03-21 12:04:14 +0100

    .env file test

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const Twitter = require('./modules/twitter');
-<<<<<<< HEAD
-
-=======
->>>>>>> 0cd2fdc07b49c34cb86a72f9a537acf7d33158c1
 // Twitter.makePictureOfTheDayPost();
 // Twitter.makeNearestEarthObjectPost();
 // Twitter.makeLatestSpaceXLaunchPost();
--- config.js
+++ config.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
     twitter: {
-        consumer_key: '3DuFQvl4qbM6BRPrViu932XFi',
-        consumer_secret: '3MhqBfu9Xi36ki6N0vg9VeIm75sVDP38bqjoUlHWYkff5Hz7TZ',
-        access_token: '1197922489130799104-AcaUsFGaIn73z74Oj86FOBu10j8pIX',
-        access_token_secret: 'a8IPVIfRERd10QrlAcZozGLOS421eLOARjN98DlYr5SqB'
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     },
     nasa: {
         api_key: '5WixNpypnqJ36Qgwdpgg7hXNzWnaMriHs6K5Eqy8'
