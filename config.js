3864b9e990b18bb5ca525092cdc570b1eecae1ed
Timur Celik <timur.celik@gmx.de>
2020-03-21 12:11:51 +0100

    modified config file with environment variables

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ module.exports = {
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     },
     nasa: {
-        api_key: '5WixNpypnqJ36Qgwdpgg7hXNzWnaMriHs6K5Eqy8'
+        api_key: process.env.NASA_API_KEY
     },
     firebase: {
-        api_key: '../twittx-9bf78-firebase-adminsdk-8w28w-eb32c75d0e.json',
-        app_url: 'https://twittx-9bf78.firebaseio.com'
+        api_key: process.env.FIREBASE_API_KEY,
+        app_url: process.env.FIREBASE_APP_URL
     }
 }
